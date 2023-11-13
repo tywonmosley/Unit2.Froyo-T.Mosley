c2c332587f6342293b9eb0c0077af7410e8eec24
tywonmosley <146139910+tywonmosley@users.noreply.github.com>
2023-11-12 19:22:12 -0500

    updated files

--- index15.js
+++ index15.js
@@ -1,7 +1,18 @@
 //Build code that prompt suers for a list of intergers searated by commas.
 const inputHereString = prompt(
-    "Input flavors here with commas.","vanilla,vanilla,vanilla,strawberry,coffee,coffee" 
+    "Input flavors here with commas seperating each choice.",
+    "vanilla,vanilla,vanilla,strawberry,coffee,coffee" 
 );
+console.log(inputHereString);
 //Split the string of flavor into an array of strings.
-const stringFlavorArr = inputHereString.split(",")
+const stringFlavorArr = inputHereString.split(",");
 console.log(stringFlavorArr);
+
+//Convert the array of strings into array of flavors
+const flavors = [];
+for (let favs = 0; favs < stringFlavorArr.length; favs++) {
+    const flavFlav = stringFlavorArr[favs];
+    const favor = flavFlav;
+    flavors.push(favor)
+} 
+console.log(flavors)
